a5a60dcadea3bafb5a7f641714524b7a33cc34fd
Julien Roussel <roussel-julien@outlook.fr>
2025-03-14 15:45:36 +0100

    Initialisation git

---
+++ back/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const connectMongoDB = require('./config/dbMongo');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const ENV = require('./config/env');
+const app = express();
+
+// IMPORT ROUTER
+const userRouter = require('./router/user.router')
+
+// CONNEXION MONGO
+connectMongoDB(ENV.MONGO_URI, ENV.DB_NAME)
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+// URLS API PREFIX
+app.use("/api/users", userRouter)
+
+// MIDDLEWARES DE GESTION D'ERROR
+app.use((error, req, res, next) => {
+    const status = error.status || 500;
+    const message = error.message || "Une erreur est survenue"
+    const details = error.details || null;
+
+    res.status(status).json({
+        error: {
+            status, 
+            message,
+            details
+        }
+    })
+})
+
+module.exports = app;
